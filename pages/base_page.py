7844ba33f095754d752043fd08c698466caf5964
Sergey Konashenkov <konashenkov-s@yandex.ru>
2022-09-25 21:24:56 +0300

    typo fixed in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -55,7 +55,7 @@ class BasePage():
 
     def should_be_authorized_user(self):
         assert self.is_element_present(*BasePageLocators.USER_ICON),\
-            "User icon is not presented, probably unauthorised user"                                                              " probably unauthorised user"
+            "User icon is not presented, probably unauthorised user"
 
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
